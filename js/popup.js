df82403b12cf6f353e91325dc11f0b4f55a09058
Lasse <l_heemann@apple.com>
2020-09-09 21:28:16 +0200

    horrible workaround to show text on osx

--- js/popup.js
+++ js/popup.js
@@ -24,7 +24,7 @@ var utils = {
 			if (localStorage.pinned === "skip") {
 				tabs = tabs.filter(function(t){ return !t.pinned; });
 			}
-			
+
 			cb(tabs.map(function(t){ return t.url; }));
 			sessions.load();
 		});
@@ -45,18 +45,18 @@ var state = {
 var sessions = {
 	list: JSON.parse(localStorage.sessions),
 	temp: localStorage.temp ? JSON.parse(localStorage.temp) : undefined,
-	
+
 	load: function(){
 		var $temp = $("#main-saved-temp"), $list = $("#main-saved-list"), $main = $("#main-saved");
 		$temp.add($list).empty();
-		
+
 		if (sessions.temp) {
 			localStorage.temp = JSON.stringify(sessions.temp);
 			$temp.html("<a>&times;</a> Temp session: " + sessions.display(null, true) + " - <a>Open</a> - <a>Add</a> (<a>tab</a>)<hr>");
 		} else {
 			delete localStorage.temp;
 		}
-		
+
 		localStorage.sessions = JSON.stringify(sessions.list);
 		$.each(sessions.list, function(name){
 			$("<div/>").html("<big>" + utils.escape(name) + "</big><a>&times;</a><br>" +
@@ -64,9 +64,9 @@ var sessions = {
 				"<span><a>Open</a> - <a>Add</a> (<a>tab</a>) - <a>Replace</a></span>" +
 			"<br><hr>").attr("data-name", name).appendTo($list);
 		});
-		
+
 		$("hr", "#main-saved").last().remove();
-		
+
 		if(Object.keys(sessions.list).length > 10) $main.addClass("scroll");
                 else $main.removeClass("scroll");
 	},
@@ -81,56 +81,56 @@ var sessions = {
 var actions = {
 	import: [function(){
 		var reader = new FileReader();
-		
+
 		reader.onload = function(e){
 			try {
 				$.each(JSON.parse(e.target.result), function(name, urls){
 					sessions.list[name] = urls;
 				});
-				
+
 				state.entered = "Success";
 			} catch (e) {
 				state.entered = "ParseError";
 			}
-			
+
 			utils.action("import", 1);
 		};
-		
+
 		reader.onerror = function(){
 			state.entered = "FileError";
 			utils.action("import", 1);
 		};
-		
+
 		reader.readAsText($("#import-file")[0].files[0]);
 	}, function(){
 		var status = state.entered,
 			success = status === "Success",
 			message = $("#import-message").text(success ? "Success!" : "Import failed!").delay(500).slideDown();
-		
+
 		success && message.delay(1500).queue(function(next){
 			location.search ? window.close() : utils.view("main");
 			message.hide();
 			next();
 		});
-		
+
 	}],
-	
+
 	export: [function(){
 		var data = new Blob([localStorage.sessions]);
-		
+
 		$("#export-link").prop("href", (window.URL || window.webkitURL).createObjectURL(data));
 	}, function(){
 		$("#export-check").fadeIn().delay(2000).fadeOut();
-		
+
 	}],
-	
+
 	rename: [function(name){
 		$("#rename-legend").html("Rename " + sessions.display(name));
 		utils.view("rename");
 		$("#rename-text").val("").focus();
 	}, function(oname){
 		var nname = state.entered = $("#rename-text").val().trim();
-		
+
 		if (nname) {
 			if (sessions.list[nname]) {
 				utils.confirm("Are you sure you want to replace " + sessions.display(nname) + " by renaming " + sessions.display(oname) + "?", 2);
@@ -141,22 +141,22 @@ var actions = {
 		}
 	}, function(oname){
 		sessions.list[state.entered] = sessions.list[oname];
-		
+
 		if (state.entered !== oname) {
 			delete sessions.list[oname];
 		}
-		
+
 	}],
-	
+
 	add: [function(name){
 		utils.confirm("Are you sure you want to add the current window's tabs to " + sessions.display(name) + "?");
 	}, function(name){
 		utils.tabs(function(tabs){
 			Array.prototype.push.apply(name === null ? sessions.temp : sessions.list[name], tabs);
 		});
-		
+
 	}],
-	
+
 	tab: [function(name){
 		utils.confirm("Are you sure you want to add the current tab to " + sessions.display(name) + "?");
 	}, function(name){
@@ -164,26 +164,26 @@ var actions = {
 			(name === null ? sessions.temp : sessions.list[name]).push(tab.url);
 			sessions.load();
 		});
-		
+
 	}],
-	
+
 	replace: [function(name){
 		utils.confirm("Are you sure you want to replace " + sessions.display(name) + " with the current window's tabs?");
 	}, function(name){
-		
+
 		utils.tabs(function(tabs){
 			sessions.list[name] = tabs;
 		});
 	}, function(name){
 		utils.confirm("Are you sure you want to replace " + sessions.display(name) + " with the session being saved?");
 	}],
-	
+
 	edit: [function(name){
 		$("#edit-legend").html("Edit " + utils.escape(name));
 
-		
+
                 var $list = $("#edit-saved-session"), $main = $("#edit-saved");
-		
+
 		$list.html("");
                 $list.attr("data-name", name)
 		$.each(sessions.list[name], function(n){
@@ -191,24 +191,24 @@ var actions = {
                         var $url = utils.escape(sessions.list[name][n]);
 			$("<div/>").html("<big><a title=\"" + $url + "\">" + $url + "</a></big><a>&times;</a><hr>").attr("data-name", sessions.list[name][n]).appendTo($list);
 		});
-		
+
 		if(Object.keys(sessions.list[name]).length > 10) $main.addClass("scroll");
                 else $main.removeClass("scroll");
-		
+
 		utils.view("edit");
 		$("#edit-text").val("").focus();
 	}, function(name){
                 var $list = $("#edit-saved-session")[0], tabs = [];
-                
+
                 name = $list.dataset.name;
 		$.each($list.children, function(n, e){
                         if((!(e.style.opacity)) || (e.style.opacity > 0.9)) tabs.push(e.dataset.name);
 		});
-                
+
 		sessions.list[name] = tabs;
 		utils.view("main");
 	}],
-	
+
 	remove: [function(name){
 		utils.confirm("Are you sure you want to remove " + sessions.display(name) + "?");
 	}, function(name){
@@ -217,7 +217,7 @@ var actions = {
 		} else {
 			delete sessions.list[name];
 		}
-		
+
 	}],
 
 	removeURL: [function(target){
@@ -236,20 +236,21 @@ var actions = {
 			background.openSession(win.id, [target.title], e, false) !== false && window.close();
 		});
 	}],
-	
+
 	savetemp: [function(){
 		utils.tabs(function(tabs){
 			sessions.temp = tabs;
 		});
-		
+
 	}],
-	
+
 	save: [function(){
-		var $name = $("#main-save-name"), name = state.name = $name.val().trim();
-		
+		let $name = $("#main-save-name"), name = state.name = $name.val().trim();
+		// $(this).addClass('refresh').removeClass('refresh');
+
 		if (name) {
 			$name.val("");
-			
+
 			utils.action("replace", sessions.list[name] ? 2 : 1);
 		}
 	}]
@@ -259,25 +260,25 @@ var actions = {
 /*** events ***/
 $("body").on("focus", "*", function(){
 	this.blur();
-	
+
 	$("body").off("focus", "*");
 }).on("click keypress", "[data-view], [data-action]", function(e){
 	if ((this.tagName === "BUTTON" && e.type === "keypress") || (this.tagName === "INPUT" && (e.type !== "keypress" || e.which !== 13))) {
 		return;
 	}
-	
+
 	"view" in this.dataset && utils.view(this.dataset.view);
 	"action" in this.dataset && utils.action(this.dataset.action, this.dataset.actionindex);
 });
 
 $("#main-saved-list").on("click", "big, div > a:not([title])", function(){
 	state.name = this.parentNode.dataset.name;
-	
+
 	utils.action(this.tagName === "BIG" ? "rename" : "remove");
 }).on("click", "span > a", function(e){
 	var action = this.textContent.toLowerCase(),
 		name = state.name = this.parentNode.parentNode.dataset.name;
-	
+
 	if (action === "open") {
 		chrome.windows.getCurrent(function(win){
 			background.openSession(win.id, sessions.list[name], e, false) !== false && window.close();
@@ -303,15 +304,23 @@ $("#edit-saved-session").on("click", "big, div > a:not([title])", function(e){
 			}
 			if (i < wins.length + 5) chrome.windows.create({ url: e.target.title }, function(win) {
 				state.id = win.id;
-			}); 
-		}); 
+			});
+		});
 	} else utils.action("removeURL");
+});
+
+$("#main-save-name").keyup(function() {
+	$("#main").hide().show(0)
+});
+
+$("#rename-text").keyup(function() {
+	$("#main").hide().show(0)
 });
 
 $("#main-saved-temp").on("click", "a:not([title])", function(e){
 	var action = this.textContent.toLowerCase();
 	state.name = null;
-	
+
 	if (action === "open") {
 		chrome.windows.getCurrent(function(win){
 			background.openSession(win.id, sessions.temp, e, true) !== false && window.close();
@@ -333,17 +342,17 @@ sessions.load();
 
 if (localStorage.readchanges !== "true") {
 	$("#main-changelog").show();
-	
+
 	localStorage.readchanges = true;
 }
 
 if (location.search) {
 	$("#import [data-view]").click(function(){
 		window.close();
-		
+
 		return false;
 	});
-	
+
 	utils.view("import");
 }
 
